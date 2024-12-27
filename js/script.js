b5c9d53d5484d79daf9cb98324c791d2170e2cc6
bernard rayoso <rayosobernard@gmail.com>
2024-12-28 00:20:46 +0800

    update headers

--- js/script.js
+++ js/script.js
@@ -50,9 +50,24 @@ async function checkout() {
 
   fetch(`https://payments.maya.ph/invoice/payment?id=${id}&amount=${amount}`, {
     method: "GET",
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-    },
+    headers: [
+      {
+        source: "https://payments.maya.ph/invoice/(.*)",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ],
   })
     .then(response => response.json())
     .then(data => console.log(data));
